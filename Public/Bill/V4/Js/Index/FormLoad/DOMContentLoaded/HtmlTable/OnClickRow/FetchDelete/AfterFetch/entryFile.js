2734d3eb1957651f68fe7c1ba585b8244a35f01b
Keshav Nalam <nknnkumar@yahoo.com>
2025-10-01 16:58:17 +0530

    delete perfect

--- Public/Bill/V4/Js/Index/FormLoad/DOMContentLoaded/HtmlTable/OnClickRow/FetchDelete/AfterFetch/entryFile.js
+++ Public/Bill/V4/Js/Index/FormLoad/DOMContentLoaded/HtmlTable/OnClickRow/FetchDelete/AfterFetch/entryFile.js
@@ -1,6 +1,7 @@
 let StartFunc = () => {
-    let jVarLocalRefreshBSTableId = document.querySelector(`.RefreshBSTableClass`);
-    jVarLocalRefreshBSTableId.click();;
+    window.location.reload();
+    // let jVarLocalRefreshBSTableId = document.querySelector(`.RefreshBSTableClass`);
+    // jVarLocalRefreshBSTableId.click();
 };
 
 export { StartFunc }
--- Public/Bill/V4/Js/Index/FormLoad/DOMContentLoaded/HtmlTable/OnClickRow/entryFile.js
+++ Public/Bill/V4/Js/Index/FormLoad/DOMContentLoaded/HtmlTable/OnClickRow/entryFile.js
@@ -18,13 +18,8 @@ const StartFunc = async (row, $element, field) => {
         if (jVarLocalFromSwal.isConfirmed) {
             if ("pk" in row) {
                 await FetchDelete({ inRowPk: row.pk });
-                Swal.fire({
-                    title: "Deleted!",
-                    text: `The record ${row.pk} has been deleted.`,
-                    icon: "success",
-                    confirmButtonColor: "#3085d6",
-                    confirmButtonText: "OK"
-                });
+
+                // window.location.reload();
             }
         }
     }
